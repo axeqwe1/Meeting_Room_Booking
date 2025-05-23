afb194a72891c1542dd52441c5ca3d5ab628659c
Bunniphat.l <shuffer2543@gmail.com>
2025-05-23 17:43:26 +0700

    adjust padding

--- src/page/RoomManagement.tsx
+++ src/page/RoomManagement.tsx
@@ -33,7 +33,7 @@ const RoomManagement: React.FC = () => {
   }
   return (
     <>
-      <div className="container mx-auto px-4 py-6">
+      <div className="container mx-auto px-4 py-4">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-2xl font-bold text-gray-800">Room Management</h1>
           <button
@@ -41,7 +41,7 @@ const RoomManagement: React.FC = () => {
               setSelectedRoom(undefined);
               setShowRoomForm(true);
             }}
-            className="hover:cursor-pointer flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors hover:cursor-pointer"
+            className="hover:cursor-pointer flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
           >
             <Plus className="h-5 w-5 mr-2" />
             Add Room
